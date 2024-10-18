8eea39fd444308faa45160b4f320b250979250a0
vldkhu <evirus777@yandex.ru>
2024-10-18 23:46:08 +0500

    create srtuct section question

--- internal/handlers/quiz.go
+++ internal/handlers/quiz.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go_quizzer/internal/models"
 	"math/rand"
+	"os"
 	"strconv"
 	"time"
 )
@@ -20,36 +21,58 @@ func GetRandomQuestions(questions []models.Question, n int) []models.Question {
 }
 
 func StartQuiz() {
-	selectedQuestions := GetRandomQuestions(models.Questions, 20)
-	score := 0
+	fmt.Println("Тестирование по общим знаниям программирования.")
+	fmt.Println("*****")
+	for {
+		selectedQuestions := GetRandomQuestions(models.Questions, 20)
+		score := 0
 
-	for _, q := range selectedQuestions {
-		fmt.Println(q.Text)
-		for i, option := range q.Options {
-			fmt.Printf("%d: %s\n", i+1, option)
+		for _, q := range selectedQuestions {
+			fmt.Println(q.Text)
+			for i, option := range q.Options {
+				fmt.Printf("%d: %s\n", i+1, option)
+			}
+
+			var userAnswer string
+			fmt.Println("*****")
+			fmt.Print("Ваш ответ (введите номер): ")
+			fmt.Scanln(&userAnswer)
+
+			// Преобразуем ответ пользователя в число
+			answerIndex, err := strconv.Atoi(userAnswer)
+			if err != nil || answerIndex < 1 || answerIndex > len(q.Options) {
+				fmt.Println("Неверный ввод. Пожалуйста, введите номер варианта.")
+				continue // Пропускаем итерацию, если ввод неверный
+			}
+
+			// Проверяем правильность ответа
+			correctAnswer := q.Answer
+			if q.Options[answerIndex-1] == correctAnswer {
+				fmt.Println("Правильно!")
+				score++
+				fmt.Println("*****************************")
+			} else {
+				fmt.Printf("Неправильно! Правильный ответ: %s\n", correctAnswer)
+				fmt.Println("*****************************")
+			}
 		}
+		percentage := (float64(score) / float64(len(selectedQuestions))) * 100
+		fmt.Println("*****")
+		fmt.Printf("Ваш итоговый счет: %d из %d\n", score, len(selectedQuestions))
+		fmt.Printf("Процент верных ответов: %.2f%%\n", percentage)
+		fmt.Println("*****")
 
-		var userAnswer string
-		fmt.Print("Ваш ответ (введите номер): ")
-		fmt.Scanln(&userAnswer)
+		fmt.Println("Тест завершен.")
+		var choice string
+		fmt.Println("Введите 'y' для выхода или 'n' для перезапуска теста:")
+		fmt.Scan(&choice)
 
-		// Преобразуем ответ пользователя в число
-		answerIndex, err := strconv.Atoi(userAnswer)
-		if err != nil || answerIndex < 1 || answerIndex > len(q.Options) {
-			fmt.Println("Неверный ввод. Пожалуйста, введите номер варианта.")
-			continue // Пропускаем итерацию, если ввод неверный
-		}
-
-		// Проверяем правильность ответа
-		correctAnswer := q.Answer
-		if q.Options[answerIndex-1] == correctAnswer {
-			fmt.Println("Правильно!")
-			score++
+		if choice == "y" {
+			os.Exit(0)
+		} else if choice == "n" {
+			fmt.Println("Перезапуск приложения...")
 		} else {
-			fmt.Printf("Неправильно! Правильный ответ: %s\n", correctAnswer)
+			fmt.Println("Неверный ввод...")
 		}
 	}
-	percentage := (float64(score) / float64(len(selectedQuestions))) * 100
-	fmt.Printf("Ваш итоговый счет: %d из %d\n", score, len(selectedQuestions))
-	fmt.Printf("Процент верных ответов: %.2f%%\n", percentage)
 }
