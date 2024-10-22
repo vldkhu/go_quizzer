a1a0e58b33f17c2cbd899f04fa7fc6c1a1f0983f
vldkhu <evirus777@yandex.ru>
2024-10-22 18:23:39 +0500

    rectruct app

--- cmd/quiz/main.go
+++ cmd/quiz/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"go_quizzer/internal/handlers"
+	"go_quizzer/internal/models"
 )
 
 func main() {
-	handlers.StartQuiz()
+	// Получение вопросов из модели
+	sections := models.GetQuestions()
+
+	// Запуск теста
+	handlers.StartQuiz(sections)
 }
+
+//TODO доработать логику структур вопросов
+//TODO создать простой restful сервис
+//TODO подумать какую БД выбрать
